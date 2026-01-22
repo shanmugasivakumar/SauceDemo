2be66bcbbd3a54aa788c10f16ccabb85ee18609c
shanmugasivakumar <shanmugalakshmi.sivakumar@gmail.com>
2026-01-22 16:13:48 +0000

    Update InventoryPage.java

--- src/main/java/com/pages/InventoryPage.java
+++ src/main/java/com/pages/InventoryPage.java
@@ -3,7 +3,8 @@ package com.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class InventoryPage {
