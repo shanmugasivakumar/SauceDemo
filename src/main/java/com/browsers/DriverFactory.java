fbd9f92638fb04a48aa5be5ceaa1866e57625ce7
shanmugasivakumar <shanmugalakshmi.sivakumar@gmail.com>
2026-01-22 07:53:26 +0000

    Update DriverFactory.java

--- src/main/java/com/browsers/DriverFactory.java
+++ src/main/java/com/browsers/DriverFactory.java
@@ -1,7 +1,9 @@
 package com.browsers;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -12,8 +14,20 @@ public class DriverFactory {
     }
 
     public static void initDriver() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
     }
 
     public static void quitDriver() {
